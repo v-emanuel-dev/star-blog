0e3b101cfe9c5593e335a03094a9fedf3ff7ce40
Victor Emanuel <v.emanuel.pacheco@gmail.com>
2024-10-24 12:14:26 -0300

    update user profile

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../models/user.model'; // Importa a interface User
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +13,34 @@ export class UserService {
     new BehaviorSubject<string | null>(null);
   public profilePicture$ = this.profilePictureSubject.asObservable();
 
-  private usersSubject = new BehaviorSubject<any[]>([]);
-  users$ = this.usersSubject.asObservable();
+  private userSubject = new BehaviorSubject<User | null>(null);
+  public user$ = this.userSubject.asObservable();
+
+  private usersSubject = new BehaviorSubject<User[]>([]);
+  public users$ = this.usersSubject.asObservable();
 
   constructor(private http: HttpClient) {
+    this.initializeUserAndProfilePicture();
+  }
+
+  private initializeUserAndProfilePicture(): void {
     const storedProfilePicture = localStorage.getItem('profilePicture');
     if (storedProfilePicture) {
       this.profilePictureSubject.next(storedProfilePicture);
+    }
+
+    const storedUser = JSON.parse(localStorage.getItem('user') || 'null');
+    this.userSubject.next(storedUser);
+  }
+
+  loadUserData(userId: number): void {
+    if (userId !== null) {
+      this.getUserById(userId).subscribe((user: User) => {
+        this.userSubject.next(user);
+        localStorage.setItem('user', JSON.stringify(user));
+      });
+    } else {
+      console.warn('User ID is null while loading user data.');
     }
   }
 
@@ -29,39 +51,29 @@ export class UserService {
   }
 
   updateUserAdmin(id: number, userData: any): Observable<any> {
-    const token = localStorage.getItem('accessToken'); // Pega o token do localStorage
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
-
-    console.log('Updating user:', { id, userData, token }); // Log do usuário que está sendo atualizado
+    const headers = this.getAuthHeaders();
+    console.log('Updating user:', { id, userData });
 
     return this.http.put(`${this.apiUrl}/admin/update/${id}`, userData, { headers }).pipe(
       tap({
-        next: (response) => {
-          console.log('Update response:', response); // Log de resposta do servidor
-
-          const updatedUsers = this.usersSubject.value.map((u) =>
-            u.id === id ? { ...u, ...userData } : u
-          );
-          this.usersSubject.next(updatedUsers);
-          console.log('Updated users list:', updatedUsers); // Log da lista de usuários atualizada
-        },
-        error: (err) => {
-          console.error('Error updating user:', err); // Log de erro caso ocorra
-        }
+        next: (response) => this.handleUserUpdate(id, userData, response),
+        error: (err) => console.error('Error updating user:', err)
       })
     );
   }
 
+  private handleUserUpdate(id: number, userData: any, response: any): void {
+    console.log('Update response:', response);
+    const updatedUsers = this.usersSubject.value.map((u) =>
+      u.id === id ? { ...u, ...userData } : u
+    );
+    this.usersSubject.next(updatedUsers);
+    console.log('Updated users list:', updatedUsers);
+  }
 
   deleteUser(userId: number): Observable<any> {
-    const token = localStorage.getItem('accessToken');
-    return this.http.delete(`${this.apiUrl}/users/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).pipe(
+    const headers = this.getAuthHeaders();
+    return this.http.delete(`${this.apiUrl}/users/${userId}`, { headers }).pipe(
       tap(() => {
         const filteredUsers = this.usersSubject.value.filter((u) => u.id !== userId);
         this.usersSubject.next(filteredUsers);
@@ -69,17 +81,14 @@ export class UserService {
     );
   }
 
-  updateProfilePicture(picture: string | null) {
+  updateProfilePicture(picture: string | null): void {
     console.log('Updating profile picture in UserService:', picture);
     localStorage.setItem('profilePicture', picture ?? '');
     this.profilePictureSubject.next(picture);
-    console.log(
-      'Profile picture stored in localStorage:',
-      localStorage.getItem('profilePicture')
-    );
+    console.log('Profile picture stored in localStorage:', localStorage.getItem('profilePicture'));
   }
 
-  getUserById(userId: number) {
+  getUserById(userId: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/users/${userId}`);
   }
 
@@ -89,35 +98,23 @@ export class UserService {
     email: string,
     password: string | null,
     selectedImage: File | null,
-    role: string, // Adicione o parâmetro role
+    role: string,
     headers: HttpHeaders
   ): Observable<any> {
+    const formData = this.createUserFormData(username, email, password, selectedImage, role);
+    console.log('Sending update user request with data:', { userId, username, email, password, selectedImage, role });
+
+    return this.http.put(`${this.apiUrl}/update/${userId}`, formData, { headers });
+  }
+
+  private createUserFormData(username: string, email: string, password: string | null, selectedImage: File | null, role: string): FormData {
     const formData = new FormData();
     formData.append('username', username);
     formData.append('email', email);
-
-    if (password) {
-      formData.append('password', password);
-    }
-
-    if (selectedImage) {
-      formData.append('profilePicture', selectedImage);
-    }
-
-    formData.append('role', role); // Adicione o role no FormData
-
-    console.log('Sending update user request with data:', {
-      userId,
-      username,
-      email,
-      password,
-      selectedImage,
-      role, // Inclua o role nos logs para depuração
-    });
-
-    return this.http.put(`${this.apiUrl}/update/${userId}`, formData, {
-      headers,
-    });
+    if (password) formData.append('password', password);
+    if (selectedImage) formData.append('profilePicture', selectedImage);
+    formData.append('role', role);
+    return formData;
   }
 
   getUserId(): string | null {
@@ -133,7 +130,7 @@ export class UserService {
   }
 
   getProfilePicture(): string | null {
-    return this.cleanUrl(localStorage.getItem('profilePicture')); // Limpa a URL ao retornar
+    return this.cleanUrl(localStorage.getItem('profilePicture'));
   }
 
   getAllUserInfo(): {
@@ -152,8 +149,12 @@ export class UserService {
 
   cleanUrl(url: string | null): string | null {
     if (!url) return null;
-    // Remove o prefixo 'http://localhost:3000/' se existir
     const prefix = 'http://localhost:3000/';
     return url.startsWith(prefix) ? url.replace(prefix, '') : url;
+  }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('accessToken');
+    return new HttpHeaders({ Authorization: `Bearer ${token}` });
   }
 }
